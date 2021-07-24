8e14d1df15889d1c7e468e4be2a823cabf289cfc
blackforklift <81161466+blackforklift@users.noreply.github.com>
2021-07-25 01:33:08 +0300

    Add files via upload

---
+++ Client_Side/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Client_Side
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.YourIPtextBox = new System.Windows.Forms.TextBox();
+            this.ConnectIPtextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(590, 492);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SEND";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 95);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(653, 381);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 492);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(552, 48);
+            this.textBox1.TabIndex = 2;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(507, 65);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 24);
+            this.ConnectButton.TabIndex = 6;
+            this.ConnectButton.Text = "BAĞLAN";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click_1);
+            // 
+            // YourIPtextBox
+            // 
+            this.YourIPtextBox.Location = new System.Drawing.Point(100, 25);
+            this.YourIPtextBox.Multiline = true;
+            this.YourIPtextBox.Name = "YourIPtextBox";
+            this.YourIPtextBox.Size = new System.Drawing.Size(387, 24);
+            this.YourIPtextBox.TabIndex = 8;
+            // 
+            // ConnectIPtextBox
+            // 
+            this.ConnectIPtextBox.Location = new System.Drawing.Point(100, 65);
+            this.ConnectIPtextBox.Multiline = true;
+            this.ConnectIPtextBox.Name = "ConnectIPtextBox";
+            this.ConnectIPtextBox.Size = new System.Drawing.Size(387, 24);
+            this.ConnectIPtextBox.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "IP Adresiniz";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Ana Bilgisayar IP";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 562);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ConnectIPtextBox);
+            this.Controls.Add(this.YourIPtextBox);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VizeCHAT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox YourIPtextBox;
+        private System.Windows.Forms.TextBox ConnectIPtextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Client_Side/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client_Side
+{
+    public partial class Form1 : Form
+    {
+        TcpClient _client;
+        byte[] _buffer = new byte[4096];
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName()); ///local ip alır ve iptextbox'a yazar
+            foreach (IPAddress adress in localIP)
+            {
+                if (adress.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    YourIPtextBox.Text = adress.ToString();
+                }
+            }
+        }
+        private void Server_MessageReceived(IAsyncResult ar)
+        {
+            if (ar.IsCompleted)
+            {
+                ///Mesajı alma kısmı
+                ///Bu kısımda mesajların hangi thread de gelip gittiğini bilmiyoruz(IAsyncResult) aslında ne zaman hangi dataya ulaştığımız önemli
+                var bytesIn = _client.GetStream().EndRead(ar); ///veriyi al okumayı bitir.
+                if (bytesIn > 0)
+                {
+                    ///bu server için gelen datadan bir string oluşturur
+                    /// datamız basit bir string ,ama başka çeşit de olabilir.(mesela Binary şeklinde)
+
+                    var tmp = new byte[bytesIn];               ///Dizimizin bir kopyasını oluştup başlama zamanlarına 0 atadık.
+                    Array.Copy(_buffer, 0, tmp, 0, bytesIn);
+                    var str = Encoding.ASCII.GetString(tmp); ///SERVER'DAN alacağımız her şey Ascii şeklinde olacak böylece farklı dilde yazılmış serverlar ile de çalışabilir bu program.
+
+                    /// Kopyaları Delegate olarak atamamız lazım ,kullanıcı arayüzünde gerçekleştirdiğmiz işlemler ana thread içinde olmalı.
+                    ///ana thread içinde olmadığımızdan BeginInvoke metodu sayesinde ana thread içine delegate atarız.
+                    BeginInvoke((Action)(() =>
+                    {
+                        listBox1.Items.Add(str);
+                        listBox1.SelectedIndex = listBox1.Items.Count - 1; ///listbox a her eklenen itemden sonra aşağı doğru hareket etsin ve yeni itemleri göstersin
+                    }));
+                }
+                // Buffer temizle tekrar dinle
+                Array.Clear(_buffer, 0, _buffer.Length);
+                _client.GetStream().BeginRead(_buffer,
+                                                0,
+                                                _buffer.Length,
+                                                Server_MessageReceived,
+                                                null);
+            }
+
+        }
+
+   
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    
+
+        private void ConnectButton_Click_1(object sender, EventArgs e) ///textboxa yazılan ip oku girilen portu al servera bağlan
+        {
+
+            _client = new TcpClient();
+            IPEndPoint IpEnd = new IPEndPoint(IPAddress.Parse(ConnectIPtextBox.Text), 80);
+            _client.Connect(IpEnd);
+
+            _client.GetStream().BeginRead(_buffer,
+                                           0,
+                                           _buffer.Length,
+                                           Server_MessageReceived,
+                                           null);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var msg = Encoding.ASCII.GetBytes(textBox1.Text);  ///mesajı ascii ye ceviriyoruz Getstream metodu ile ağda data alış-verişi yapıyoruz.
+            _client.GetStream().Write(msg, 0, msg.Length);
+
+            textBox1.Text = "";   ///SEND 'e basınca textbox daki mesajı temizle ve odağı textbox a al
+            textBox1.Focus();
+        }
+    }
+}
